917c8e5154bba459af626e66f84c7550747d1045
2rohyun <dlehgus5656@naver.com>
2021-05-03 21:11:40 +0900

    first commit

--- insta.py
+++ insta.py
@@ -1,74 +1,107 @@
-from selenium import webdriver
 from urllib.request import urlopen
 from urllib.parse import quote_plus
-from selenium.webdriver.chrome.options import Options
-from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException as NOERROR
 import time
-import datetime
-import csv
-import urllib.parse
-import urllib.request
+import pandas as pd
+import pandas_csv
 
 # 인스타그램 태그를 입력하여 해당 태그에 대한 검색 결과 웹 페이지를 띄움.
 baseUrl = 'https://www.instagram.com/'
-plusUrl = input('검색할 user_id를 입력하세요 : ')
+plusUrl = input('검색할 태그를 입력하세요 : ')
 url = baseUrl + quote_plus(plusUrl)
-driver = webdriver.Chrome() # 크롬 웹 드라이버
-driver.get(url) # 크롬에서 url를 가져옴
 
-driver.implicitly_wait(3) # 페이지가 로딩될 때 까지 암묵적으로 기다린다.
-# 로그인 버튼을 누르고 ID 와 PASSWORD를 입력한 후 로그인 버튼을 다시 누른다.
+driver = webdriver.Chrome() 
+driver.get(url) 
+driver.implicitly_wait(10) # 페이지가 로딩될 때 까지 암묵적으로 기다린다.
+
 driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[3]/div/span/a[1]/button').click() 
-driver.find_element_by_name('username').send_keys("dlehgus5656@naver.com")
-driver.find_element_by_name('password').send_keys("akdntm1212!")
-driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]').click()
-driver.find_element_by_class_name('_9AhH0').click()
+driver.find_element_by_name('username').send_keys("") # 본인 인스타그램 아이디 입력 
+driver.find_element_by_name('password').send_keys("") # 본인 인스타그램 비밀번호 입력
+driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div').click()
 
-while True: 
-    time.sleep(1)
+time.sleep(3) # 로그인 대기시간
 
-    overlap_photo = driver.find_elements_by_class_name("Yi5aA")
-    
-    if(len(overlap_photo) == 0):
-        try:
-            photo = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[1]/div/div/div[1]/div[1]/img').get_attribute('src')
-            photo_time = driver.find_element_by_class_name('_1o9PC').get_attribute('datetime')
-            photo_time = photo_time.split('.')
-            photo_time = photo_time[0].replace('-','_').replace('T','_').replace(':','_')
-            urllib.request.urlretrieve(photo, photo_time + ".jpg")
-        except NoSuchElementException:
-            photo = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[1]/div/div/div[1]/img').get_attribute('src')
-            photo_time = driver.find_element_by_class_name('_1o9PC').get_attribute('datetime')
-            photo_time = photo_time.split('.')
-            photo_time = photo_time[0].replace('-','_').replace('T','_').replace(':','_')
-            urllib.request.urlretrieve(photo, photo_time + ".jpg")
+driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+#driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
+#driver.get(url)
 
-    else:
-        for n in range(len(overlap_photo)):
-            try:
-                video = driver.find_element_by_class_name('PyenC')
-                break
-            except NoSuchElementException:
-                temp = n + 1 
-                if(temp != len(overlap_photo)): # 묶음사진 중 마지막 사진 전 사진들의 xpath
-                    photo = driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[1]/div/div/div[2]/div/div/div/div/ul/li["+str(temp)+"]/div/div/div/div/div[1]/div[1]/img").get_attribute('src')
-                else: # 묶음사진 중 마지막 사진의 xpath
-                    photo = driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[1]/div/div/div[2]/div/div/div/div/ul/li["+str(temp)+"]/div/div/div/div/div[1]/img").get_attribute('src')
-                photo_time = driver.find_element_by_class_name('_1o9PC').get_attribute('datetime')
-                photo_time = photo_time.split('.')
-                photo_time = photo_time[0].replace('-','_').replace('T','_').replace(':','_')
-                urllib.request.urlretrieve(photo, photo_time + '_' +str(temp)+ '_' + '.jpg')
+post_number = 1
 
-                try:
-                    driver.find_element_by_class_name('coreSpriteRightChevron').click()
-                except NoSuchElementException:
-                    break
+follower = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span')
+follow_num = follower.text
+print("number of followers : ", follow_num) # 팔로워 수 
 
+driver.find_element_by_class_name("_9AhH0").click()
+
+while True:
     try:
-        nextbtn = driver.find_element_by_class_name('_65Bje.coreSpriteRightPaginationArrow')
-        if(nextbtn.text == "다음"):
-            nextbtn.click()
+        loc = driver.find_element_by_class_name("O4GlU")
+        location = loc.text
+        print("location in this post : ", location) # 게시물의 위치 
+        print("\n")
     except:
-        break
+        location = " - "
+        print("no location in this post")
 
-driver.close()
+    date = driver.find_element_by_class_name("_1o9PC.Nzb55")
+    post_date = date.get_attribute('title')
+    print("date in this post : ", post_date) # 게시물 날짜 
+    
+    try:
+        matched_elements = driver.find_elements_by_class_name("xil3i")
+        hashtags = []
+
+        for matched_element in matched_elements:
+            hashtag = matched_element.text
+            hashtags.append(hashtag)
+        print(hashtags) # 게시물 해시태그 
+
+    except NOERROR:
+        print("No hashtags in this post")
+    
+    try:
+        post_write = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/div[1]/ul/div/li/div/div/div[2]/span')
+        post = post_write.text
+        print(post) # 게시물에 포함된 게시글 
+    except NOERROR:
+        post = " - "
+        print("no post write in this post")
+
+    
+    try:
+        likes_other = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[2]/div/div[2]/button/span')
+        likes_num = likes_other.text
+        print("number of likes : ", likes_num) # 게시물 좋아요 수 
+        print("\n")  
+    except NOERROR:
+        try:
+            likes = driver.find_element_by_xpath('/html/body/div[5]/div[2]/div/article/div[3]/section[2]/div/div/button/span')
+            likes_num = likes.text
+            print("number of likes : ", likes_num)
+            print("\n")
+        except NOERROR:
+            likes_num = 0
+            print("this is video")
+    
+    insert_data = {"user id" : plusUrl,
+                   "number of followers" : follow_num,
+                   "post number" : post_number,
+                   "likes" : likes_num,
+                   "location" : location,
+                   "date time" : post_date,
+                   "post write" : post,
+                   "hashtags" : [hashtags]}
+    pandas_csv.to_csv(insert_data)
+    post_number += 1
+    print("게시물 번호 : ", post_number)
+    try:
+        nextbtn = driver.find_element_by_class_name(
+            "_65Bje.coreSpriteRightPaginationArrow"
+        )
+        if nextbtn.text in ("다음", "Next"):
+            nextbtn.click()
+    except:  
+        break
